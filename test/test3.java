83b60a3bcede6a43f265ccebc83fe5f02f6c16ee
Pooria Azimi <pooriaazimi@me.com>
2012-07-20 20:44:49 +0430

    removed old files
    Signed-off-by: Pooria Azimi <pooriaazimi@me.com>

--- test/test3.java
+++
@@ -1,5 +0,0 @@
-class Factorial {
-  public static void main(String[] args) {
-    
-  }
-}
--- test/test4.java
+++
@@ -1,5 +0,0 @@
-class Factorial {
-  public static void main(String[] args) {
-    args[0] = args[1];
-  }
-}
