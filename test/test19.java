bb4fb9067155739f8a70d4144dd28c92929bba96
Pooria Azimi <pooriaazimi@me.com>
2012-07-29 22:08:46 +0430

    test19; add links to github profiles
    Signed-off-by: Pooria Azimi <pooriaazimi@me.com>

---
+++ test/test19.java
@@ -0,0 +1,21 @@
+class Factorial {
+    public static void main(String[] args) {
+        System.out.println(1);
+        if (1<2 && true)
+            alpha = b;
+        else {
+            System.out.println(1+2);
+        }
+    }
+}
+
+class Fact extends AnotherClass {
+    public int func() {
+        return 0;
+    }
+
+    public int ComputeFac(int num) {
+        new Fact(10+2).length;
+        return num_aux;
+    }
+}
